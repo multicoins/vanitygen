38a900a0d81205450588f31460071d7a79baeb25
3s3s <ivanivanovkzv@gmail.com>
2017-07-06 02:53:59 +0500

    add Marycoin

--- vanitygen.c
+++ vanitygen.c
@@ -312,6 +312,7 @@ usage(const char *name)
 "-k            Keep pattern and continue search after finding a match\n"
 "-1            Stop after first match\n"
 "-N            Generate namecoin address\n"
+"-M            Generate Marycoin address\n"
 "-T            Generate bitcoin testnet address\n"
 "-X <version>  Generate address with the given version\n"
 "-F <format>   Generate address with the given format (pubkey or script)\n"
@@ -361,7 +362,7 @@ main(int argc, char **argv)
 
 	int i;
 
-	while ((opt = getopt(argc, argv, "vqnrik1eE:P:NTX:F:t:h?f:o:s:")) != -1) {
+	while ((opt = getopt(argc, argv, "vqnrik1eE:P:NMTX:F:t:h?f:o:s:")) != -1) {
 		switch (opt) {
 		case 'v':
 			verbose = 2;
@@ -388,6 +389,11 @@ main(int argc, char **argv)
 			addrtype = 52;
 			privtype = 180;
 			scriptaddrtype = -1;
+			break;
+		case 'M':
+			addrtype = 50;
+			privtype = 129;
+			scriptaddrtype = 6;
 			break;
 		case 'T':
 			addrtype = 111;
